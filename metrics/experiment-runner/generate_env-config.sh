7b4d1db3e9d5eb5d72b0410c93b83591480ded24
Thomas Südbröcker <thomassuedbroecker@users.noreply.github.com>
2023-04-18 17:06:14 +0200

    interim fix - for - reranker passages - starting with docker compose

--- metrics/experiment-runner/generate_env-config.sh
+++ metrics/experiment-runner/generate_env-config.sh
@@ -5,23 +5,23 @@
 ########################################
 cat <<EOF
 # Microservice endpoint and extraction
-export endpoint="${endpoint}"
-export api_url="${api_url}"
-export username="${username}"
-export password="${password}"
-export verify_answer="${verify_answer}"
+export endpoint=${endpoint}
+export api_url=${api_url}
+export username=${username}
+export password=${password}
+export verify_answer=${verify_answer}
 # inputs
-export input_excel_filename="${input_excel_filename}"
-export input_folder_name="${input_folder_name}"
-export input_folder_name_qa_service_log="${input_folder_name_qa_service_log}"
+export input_excel_filename=${input_excel_filename}
+export input_folder_name=${input_folder_name}
+export input_folder_name_qa_service_log=${input_folder_name_qa_service_log}
 # outputs
-export output_question_resp_anwser_excel="${output_question_resp_anwser_excel}"
-export output_question_resp_anwser="${output_question_resp_anwser}"
-export output_error_log="${output_error_log}"
-export output_session_id="${output_session_id}"
-export output_folder_name="${output_folder_name}"
-export number_of_retries="${number_of_retries}"
-export container_run="${container_run}"
+export output_question_resp_anwser_excel=${output_question_resp_anwser_excel}
+export output_question_resp_anwser=${output_question_resp_anwser}
+export output_error_log=${output_error_log}
+export output_session_id=${output_session_id}
+export output_folder_name=${output_folder_name}
+export number_of_retries=${number_of_retries}
+export container_run=${container_run}
 # NOT NEEDED INSIDE EVALUATE APPLICATION
 # export host_ip="${number_of_retries}"
 EOF
