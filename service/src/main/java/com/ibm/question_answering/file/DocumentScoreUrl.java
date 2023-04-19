e895ad62bc0b6bd7e4d986da2887cf1951406ef2
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2023-04-19 13:14:30 +0200

    add urls

---
+++ service/src/main/java/com/ibm/question_answering/file/DocumentScoreUrl.java
@@ -0,0 +1,8 @@
+package com.ibm.question_answering.file;
+
+import com.ibm.question_answering.reranker.DocumentScore;
+
+public class DocumentScoreUrl {
+    public DocumentScore documentScore;
+    public String url;
+}
