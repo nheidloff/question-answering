08917fe964eee78f42da0360520b19b395fba3da
Thomas Südbröcker <thomassuedbroecker@users.noreply.github.com>
2023-04-19 12:10:53 +0200

    update compose configuration and add new local automation

---
+++ scripts/maas_local.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ~/.env_profile
+
+echo "*************************"
+echo "HOME_PATH: ${GLOBAL_HOME_PATH}"
+echo "*************************"
+
+verify=""
+
+if [[ "${GLOBAL_HOME_PATH}" == "${verify}" ]]; then
+    cd $(pwd)/../mass-mock
+    echo "Current path: $(pwd)"
+else
+    cd ${GLOBAL_HOME_PATH}/../service
+    echo "Current path: $(pwd)"
+fi
+
+echo "**************************"
+echo "Start maas-mock"
+echo "**************************"
+mvn package
+mvn quarkus:dev
+
---
+++ scripts/start_apps_demo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "************************************"
+echo "                 Start" 
+echo "- 'QA-Service' as local Quarkus application"
+echo "- 'Experiment-runner' as Python application"
+echo "************************************"
+
+export HOME_PATH=$(pwd)
+export SESSION_ID=$(date +%s)
+echo "************************************"
+echo "Environment configuration"
+echo "************************************"
+echo "- HOME_PATH :          ${HOME_PATH}"
+echo "- SESSION_ID:          ${SESSION_ID}"
+
+# Environment configuration save in '~/.env_profile'"
+"/bin/sh" "${HOME_PATH}"/generate_env_profile.sh > ~/.env_profile
+# cat ~/.env_profile
+
+# bash scripts
+export exp_runner="${HOME_PATH}/exp_runner_local.sh"
+export qa_service="${HOME_PATH}/qa_local.sh"
+export maas_service="${HOME_PATH}/maas_local.sh"
+
+# Enable bash automation for execution"
+chmod 755 ${exp_runner}
+chmod 755 ${qa_service}
+chmod 755 ${maas_service}
+
+
+echo "************************************"
+echo "    Open application terminals"
+echo "************************************"
+echo "- QA Service"
+open -a Terminal ${qa_service}
+sleep 10
+echo "- Experiment runner"
+open -a Terminal ${exp_runner}
+
