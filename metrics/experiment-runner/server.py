5ff80a85b5bbefb5199d07e948d01f7c11fcd254
Thomas Südbröcker <thomassuedbroecker@users.noreply.github.com>
2023-04-20 11:27:48 +0200

    insert basic functionality to flask server

--- metrics/experiment-runner/server.py
+++ metrics/experiment-runner/server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import json
-import subprocess
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from flask import Flask
 from flask_restful import Resource, Api
@@ -9,20 +10,59 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Start(Resource):
-    def get():
-        # print(subprocess.check_output(['ls', '-l']))
-        return_value = "Can't start anything!"
+class GetEnv(Resource):
+    def get(self):
+        # Customm debug information
+        print("INFO 1")
+        app_debug_channel = os.environ.get("app_debug_channel")
+        print("INFO 2")
+        # qa service
+        endpoint = os.environ.get("endpoint")
+        api_url = os.environ.get("api_url")
+        username=os.environ.get("username")
+        password=os.environ.get("password")
+        verify_answer=os.environ.get("verify_answer")
+        
+        print("INFO 3")
+        # input
+        input_excel_filename = os.environ.get("input_excel_filename")
+        input_folder_name = os.environ.get("input_folder_name")
+        input_folder_name_qa_service_metrics = os.environ.get("input_folder_name_qa_service_metrics")
+        container_run = os.environ.get("container_run")
+
+        # output
+        output_question_resp_anwser_excel = os.environ.get("output_question_resp_anwser_excel")
+        output_error_log = os.environ.get("output_error_log")
+        output_session_id = os.environ.get("output_session_id")
+        output_folder_name = os.environ.get("output_folder_name")
+        number_of_retries = os.environ.get("number_of_retries")
+        print("INFO 4")
+        return_value = str(output_question_resp_anwser_excel) + ";" +  \
+                       str(output_error_log) + ";" +  \
+                       str(output_session_id) + ";" + \
+                       str(output_folder_name) + ";" + \
+                       str(number_of_retries) + ";" + \
+                       str(input_excel_filename) + ";" + \
+                       str(input_folder_name) + ";" + \
+                       str(input_folder_name_qa_service_metrics) + ";" + \
+                       str(container_run) + ";" + \
+                       str(endpoint) + ";" + \
+                       str(api_url) + ";" + \
+                       str(username) + ";" + \
+                       str(password) + ";" + \
+                       str(verify_answer) + ";" + \
+                       str(app_debug_channel)
+               
         return return_value
 
 class Alive(Resource):
-    def get():
-        # print(subprocess.check_output(['ls', '-l']))
+    def get(self):
+
         return_value = "Yes, I am alive!"
         return return_value
 
 api.add_resource(Alive, '/')
-api.add_resource(Start, '/start')
+api.add_resource(GetEnv, '/get_env')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8084)
+    app.run(debug=False, port=8084)
