d8b44500745506ec26a09e2b45ca73c2e1ed50ee
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2023-05-04 08:29:12 +0200

    maas streaming

--- service/src/main/java/com/ibm/question_answering/maas/Answer.java
+++ service/src/main/java/com/ibm/question_answering/maas/Answer.java
@@ -3,4 +3,5 @@ package com.ibm.question_answering.maas;
 public class Answer {
     public String model_id;
     public Result[] results;
+    public String created_at;
 }
--- service/src/main/java/com/ibm/question_answering/maas/ModelAsAService.java
+++ service/src/main/java/com/ibm/question_answering/maas/ModelAsAService.java
@@ -1,6 +1,7 @@
 package com.ibm.question_answering.maas;
 
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -8,6 +9,8 @@ import javax.ws.rs.Path;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestStreamElementType;
+import io.smallrye.mutiny.Multi;
 
 @Path("")
 @Produces("application/json")
@@ -20,4 +23,8 @@ public interface ModelAsAService {
     
     @POST
     com.ibm.question_answering.maas.Answer ask(Input input);
+
+    @POST
+    @RestStreamElementType(MediaType.APPLICATION_JSON)
+    Multi<String> askAsStream(Input input);
 }
