cd1cbf9e30fb93d6c6713511d0856a78666c0c6c
Thomas Südbröcker <thomassuedbroecker@users.noreply.github.com>
2023-05-09 23:02:10 +0200

    update automation

--- scripts/ocp-deployment/deploy-to-openshift.sh
+++ scripts/ocp-deployment/deploy-to-openshift.sh
@@ -79,49 +79,14 @@ function install_helm_chart () {
     echo "*********************"
     echo ""
 
+    "/bin/sh" ./generate_values_file.sh > ./charts/question-answering-helm/values.yaml
+
     cd $HOME_PATH/charts
     TEMP_PATH_EXECUTION=$(pwd)
-    
     helm dependency update ./question-answering-helm/
-    helm install --dry-run \
-         --debug helm-test \
-         --set pullsecret.PULLSECRET=${PULL_SECRET}  \
-         --set container_registry.CR_RESOURCE_GROUP=${CR_RESOURCE_GROUP}  \
-         --set container_registry.CR_REGION=${CR_REGION}  \
-         --set container_registry.CR=${CR}  \
-         --set container_registry.CR_REPOSITORY=${CR_REPOSITORY}  \
-         --set container_image.CI_NAME=${CI_NAME}  \
-         --set container_image.CI_TAG=${CI_TAG}  \
-         --set qa.QA_API_KEY=${QA_API_KEY}  \
-         --set discovery.DISCOVERY_API_KEY=${DISCOVERY_API_KEY}  \
-         --set discovery.DISCOVERY_URL=${DISCOVERY_URL}  \
-         --set discovery.DISCOVERY_INSTANCE=${DISCOVERY_INSTANCE}  \
-         --set discovery.DISCOVERY_PROJECT=${DISCOVERY_PROJECT}  \
-         --set discovery.DISCOVERY_COLLECTION_ID=${DISCOVERY_COLLECTION_ID}  \
-         --set prime_qa.PRIME_QA_URL=${PRIME_QA_URL}  \
-         --set reranker.RERANKER_URL=${RERANKER_URL}  \
-         --set maas.MAAS_URL=${MAAS_URL}  \
-         --set maas.MAAS_API_KEY=${MAAS_API_KEY}  \
-         --set proxy.PROXY_URL=${PROXY_URL}  \
-         --set proxy.PROXY_API_KEY=${PROXY_API_KEY}  \
-         --set experiment.EXPERIMENT_METRICS_SESSION=${EXPERIMENT_METRICS_SESSION}  \
-         --set experiment.EXPERIMENT_LLM_NAME=${EXPERIMENT_LLM_NAME}  \
-         --set experiment.EXPERIMENT_LLM_MIN_NEW_TOKENS=${EXPERIMENT_LLM_MIN_NEW_TOKENS}  \
-         --set experiment.EXPERIMENT_LLM_MAX_NEW_TOKENS=${EXPERIMENT_LLM_MAX_NEW_TOKENS}  \
-         --set experiment.EXPERIMENT_LLM_MAX_INPUT_DOCUMENTS=${EXPERIMENT_LLM_MAX_INPUT_DOCUMENTS}  \
-         --set experiment.EXPERIMENT_LLM_PROMPT=${EXPERIMENT_LLM_PROMPT}  \
-         --set experiment.EXPERIMENT_RERANKER_MAX_INPUT_DOCUMENTS=${EXPERIMENT_RERANKER_MAX_INPUT_DOCUMENTS}  \
-         --set experiment.EXPERIMENT_RERANKER_MODEL=${EXPERIMENT_RERANKER_MODEL}  \
-         --set experiment.EXPERIMENT_RERANKER_ID=${EXPERIMENT_RERANKER_ID}  \
-         --set experiment.EXPERIMENT_DISCOVERY_MAX_OUTPUT_DOCUMENTS=${EXPERIMENT_DISCOVERY_MAX_OUTPUT_DOCUMENTS}  \
-         --set experiment.EXPERIMENT_DISCOVERY_CHARACTERS=${EXPERIMENT_DISCOVERY_CHARACTERS}  \
-         --set experiment.EXPERIMENT_DISCOVERY_FIND_ANSWERS=${EXPERIMENT_DISCOVERY_FIND_ANSWERS}  \
-         --set experiment.EXPERIMENT_METRICS_DIRECTORY=${EXPERIMENT_METRICS_DIRECTORY }  \
-         ./question-answering-helm/
-
-    helm lint
-    # helm install $HELM_RELEASE_NAME ./question-answering-helm
-        
+    helm install --dry-run --debug question-answering-helm ./question-answering-helm/
+    helm install question-answering-helm ./question-answering-helm/
+    
     cd $HOME_PATH
 }
 
