5907c69b01017eb1a3f9775ebe6f8fe261b0e544
Thomas Südbröcker <thomassuedbroecker@users.noreply.github.com>
2023-06-29 16:54:02 +0200

    Needs to be verified related to API changes

--- service/src/main/java/com/ibm/question_answering/maas/Parameters.java
+++ service/src/main/java/com/ibm/question_answering/maas/Parameters.java
@@ -2,6 +2,8 @@ package com.ibm.question_answering.maas;
 
 public class Parameters {
 
+    // MaaS Problem
+    // public final static double DEFAULT_TEMPERATURE = 0.7;
     public final static double DEFAULT_TEMPERATURE = 0;
     public final static int DEFAULT_MIN_NEW_TOKENS = 10;
     public final static int DEFAULT_MAX_NEW_TOKENS = 25;
@@ -9,12 +11,17 @@ public class Parameters {
     public Parameters() {
         this.max_new_tokens = DEFAULT_MAX_NEW_TOKENS;
         this.min_new_tokens = DEFAULT_MIN_NEW_TOKENS;
+
+        // MaaS problem
+        // this.temperature = 0.7;
         this.temperature = DEFAULT_TEMPERATURE;
     }
 
     public Parameters(double temperature, int max_new_tokens, int min_new_tokens) {
         this.max_new_tokens = max_new_tokens;
         this.min_new_tokens = min_new_tokens;
+        // MaaS problem        
+        // this.temperature = 0.7;       
         this.temperature = temperature;
         this.stream = false;
     }
--- service/src/main/java/com/ibm/question_answering/tgis/Sampling.java
+++ service/src/main/java/com/ibm/question_answering/tgis/Sampling.java
@@ -1,5 +1,7 @@
 package com.ibm.question_answering.tgis;
 
 public class Sampling {
+    // MaaS problem
+    // public float temperature = 0.7;
     public float temperature = 0;
 }
